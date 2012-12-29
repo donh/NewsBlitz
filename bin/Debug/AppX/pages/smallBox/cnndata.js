e0bb1eb204da2fc1c503d15ad12ddd122f412358
Don <don@workspace.tw>
2012-12-30 03:23:49 +0800

    4 data complete (Bloomberg, CNN, Economist, New York Times)

---
+++ bin/Debug/AppX/pages/smallBox/cnndata.js
@@ -0,0 +1,14 @@
+﻿var cnnData = new WinJS.Binding.List([
+//        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/60Banana.png", style: "color:#000;" },
+//        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/60Lemon.png", style: "width:100%;height:10px;background:#fff" },
+    { title: "Top Stories", url: "http://rss.cnn.com/rss/edition.rss" },
+	{ title: "World Business", url: "http://rss.cnn.com/rss/edition_business.rss" },
+    { title: "Business 360", url: "http://rss.cnn.com/rss/edition_business360.rss" },
+    { title: "World", url: "http://rss.cnn.com/rss/edition_world.rss" },
+    { title: "Africa", url: "http://rss.cnn.com/rss/edition_africa.rss" },
+    { title: "Americas", url: "http://rss.cnn.com/rss/edition_americas.rss" },
+    { title: "Asia", url: "http://rss.cnn.com/rss/edition_asia.rss" },
+    { title: "Europe", url: "http://rss.cnn.com/rss/edition_europe.rss" },
+    { title: "Middle East", url: "http://rss.cnn.com/rss/edition_meast.rss" },
+    { title: "U.S.", url: "http://rss.cnn.com/rss/edition_us.rss" },
+]);
---
+++ pages/smallBox/economistdata.js
@@ -0,0 +1,17 @@
+﻿var economistData = new WinJS.Binding.List([
+//        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/60Banana.png", style: "color:#000;" },
+//        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/60Lemon.png", style: "width:100%;height:10px;background:#fff" },
+
+     { title: "The world this week", url: "http://www.economist.com/rss/the_world_this_week_rss.xml" },
+     { title: "Briefings", url: "http://www.economist.com/feeds/print-sections/102/briefings2.xml" },
+     { title: "Letters", url: "http://www.economist.com/feeds/print-sections/68/letters.xml" },
+     { title: "Leaders", url: "http://www.economist.com/feeds/print-sections/69/leaders.xml" },
+     { title: "Business", url: "http://www.economist.com/feeds/print-sections/77/business.xml" },
+     { title: "International", url: "http://www.economist.com/feeds/print-sections/74/international.xml" },
+     { title: "Finance and economics", url: "http://www.economist.com/feeds/print-sections/79/finance-and-economics.xml" },
+     { title: "Asia", url: "http://www.economist.com/feeds/print-sections/73/asia.xml" },
+     { title: "Europe", url: "http://www.economist.com/feeds/print-sections/75/europe.xml" },
+     { title: "Middle East and Africa", url: "http://www.economist.com/feeds/print-sections/99/middle-east-africa.xml" },
+     { title: "United States", url: "http://www.economist.com/feeds/print-sections/71/united-states.xml" },
+     { title: "Indicators", url: "http://www.economist.com/feeds/print-sections/84/indicators.xml" },
+]);
