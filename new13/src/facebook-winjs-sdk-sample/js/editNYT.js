2d8131d224322bd583353653b9a4174b396530e6
iamblue <tonyone0902@gmail.com>
2012-12-29 14:31:21 +0800

    12

---
+++ new13/src/facebook-winjs-sdk-sample/js/editNYT.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+    var page = WinJS.UI.Pages.define("/pages/editNYT.html", {
+        ready: function (element, options) {
+            // Initialize the ListView
+            WinJS.UI.processAll(element);
+        }
+    });
+    var showuserNYTdata = new WinJS.Binding.List([
+     { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/60Banana.png", style: "color:#000;" },
+     { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/60Lemon.png", style: "width:100%;height:10px;background:#fff" },
+
+    ]);
+    var controlClass = WinJS.Class.define(
+        function Control_ctor(element) {
+            this.element = element || document.createElement("div");
+            this.element.winControl = this;
+
+            this.element.textContent = "Hello, World!"
+        });
+    WinJS.Namespace.define("Contoso.UI", {
+        HelloWorld: controlClass
+    });
+})();
---
+++ new13/src/facebook-winjs-sdk-sample/js/facebookdata.js
@@ -0,0 +1,10 @@
+﻿/*var facebookData = new WinJS.Binding.List([
+    //{ title: "Banana Blast", src: "http://www.yahoo.com.tw", text: "Low-fat frozen yogurt", picture: "/images/60Banana.png", style: "color:#000;" },
+    { title: "Lavish Lemon Ice", src: "http://www.yahoo.com.tw", text: "Sorbet", picture: "/images/60Lemon.png", style: "width:100%;height:10px;background:#fff" }
+]);*/
+var facebookData = new WinJS.Binding.List([
+   { "title": "hihi2", "text": "yaya" }
+]);
+
+
+//facebookData.push({ "title": "hihi2", "text": "yaya" });
